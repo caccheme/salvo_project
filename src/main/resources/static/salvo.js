840101091bcbece3302187eeffe5738c248c7377
Charity Howell <caccheme@hotmail.com>
2016-06-22 13:27:38 -0500

    make game dates each one hour apart

--- src/main/resources/static/salvo.js
+++ src/main/resources/static/salvo.js
@@ -6,14 +6,9 @@ $(document).ready(function(){
       var games = [];
 
       $.each(data, function(key, val) {
-//        var date = (new Date(val.date_created));
-        var formattedDate = new Date(val.date_created);
-        var d = formattedDate.getDate();
-        var m =  formattedDate.getMonth();
-        m += 1;  // JavaScript months are 0-11
-        var y = formattedDate.getFullYear();
+        var date = (new Date(val.date_created + (60*60*1000*key)));
 
-        games.push('<li id="' + key + '">Date Created:    ' + d + "-" + m + "-" + y + '</li>');
+        games.push('<li id="' + key + '">Date Created:    ' + date + '</li>');
       });
 
       $('<ol/>', {
