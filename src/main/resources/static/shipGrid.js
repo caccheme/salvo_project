97f88d8e5b2bc943b4aba9cccd0700771448a81c
Charity Howell <caccheme@hotmail.com>
2016-07-16 12:08:42 -0500

    create salvo Grid in game.html

--- src/main/resources/static/shipGrid.js
+++ src/main/resources/static/shipGrid.js
@@ -13,7 +13,19 @@ $(document).ready(function(){
                    // since we are using jQuery, you don't need to parse response
                    shipData = flattenArray(shipData.shipLocations);
 //                   console.log(shipData);
-                   tableCreate(shipData);
+                   tableCreate1(shipData);
+
+        }
+  });//end ajax
+
+//get salvoLocation data for a specific gamePlayer_Id
+  $.ajax({
+        method: "get",
+        url: '/api/gpSalvoLocations/'+ gamePlayer_Id,
+        dataType: 'json',
+        success: function(salvoData, textStatus, jqXHR) {
+                   salvoData = flattenArray(salvoData.salvoLocations);
+                   tableCreate2(salvoData);
 
         }
   });//end ajax
@@ -48,8 +60,8 @@ $(document).ready(function(){
         $("#player_email").text(string);
     }
 
-     function tableCreate(shipData) {
-         var body = document.getElementsByTagName('body')[0];
+     function tableCreate1(shipData) {
+         var body = document.getElementsByTagName('div')[1];
          var tbl = document.createElement('table');
          tbl.style.width = '35%';
          tbl.setAttribute('border', '1');
@@ -148,6 +160,72 @@ $(document).ready(function(){
 //  - for each row (from 1 to 10)
 //    — for each column (from A to J)
 //        if cell contains ship return HTML for a ship color block else return HTML for an empty cell
+
+     function tableCreate2(salvoData) {
+         var body = document.getElementsByTagName('div')[2];
+         var tbl = document.createElement('table');
+         tbl.style.width = '35%';
+         tbl.setAttribute('border', '1');
+         tbl.setAttribute('text-align', 'center');
+         var tbdy = document.createElement('tbody');
+         for (var i = 0; i < 11; i++) {
+             var tr = document.createElement('tr');
+
+             for (var j = 0; j < 11; j++) {
+                 if (j==0 && i==0){
+                    var td = document.createElement('td')
+                        td.appendChild(document.createTextNode(""))
+                        tr.appendChild(td)
+                 }
+                 if (j==0 && i!=0) {
+                   // output row header A-J for grid
+                   var td = document.createElement('td');
+                        td.appendChild(document.createTextNode(i))
+                            tr.appendChild(td)
+
+                 }
+                 if (i==0 && j!=0) {
+                 // output column header 1-10 for grid
+                    var td = document.createElement('td');
+                        td.appendChild(document.createTextNode(myAlphabetFunction(j)))
+                           tr.appendChild(td)
+                 }
+                 // quit making grid after made 11x11
+                 if (i == 11 && j == 11) {
+                     break
+                 }
+                 else if (i > 0 && j > 0) {
+                     var td = document.createElement('td');
+                     //loop through game data and fill in grid where ships are ... still to do
+                     //the param obj search should give the gp id value and can use it in this loop
+
+
+                    // create string values based on table position
+                    var jArray = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]; //so jArray[1] should = "A", etc
+
+                    function myConcatFunction(j, i) {
+                        var str1 = jArray[j];
+                        var str2 = i.toString();
+                        var res = str1.concat(str2);
+                        return res
+                    }
+
+                    var cellString = myConcatFunction(j,i);
+
+                    //  loop over rest of grid, check for shipLocations, mark matching locations blue
+//                    td.appendChild(document.createTextNode(cellString)); // mark cells so can visually check ship locations
+                    if (checkLocations(salvoData, cellString) == true) {
+                       td.style.backgroundColor = "orange"
+                    }
+
+                    tr.appendChild(td)
+                 }
+             }
+             tbdy.appendChild(tr);
+         }
+         tbl.appendChild(tbdy);
+         body.appendChild(tbl)
+     }
 
 
 });
