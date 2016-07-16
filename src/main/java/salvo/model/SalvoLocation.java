891ec49bf2247030b89e196cd54a24294a5b5f17
Charity Howell <caccheme@hotmail.com>
2016-07-16 11:03:25 -0500

    create SalvoLocation and SalvoLocationRepository classes

---
+++ src/main/java/salvo/model/SalvoLocation.java
@@ -0,0 +1,51 @@
+package salvo.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+public class SalvoLocation {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    long id;
+    private String salvoLocationCell;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name="salvo_id")
+    private Salvo salvo;
+
+    public SalvoLocation() { }
+
+    public SalvoLocation(Ship ship, String shipLocationCell) {
+        this.salvo = salvo;
+        this.salvoLocationCell = salvoLocationCell;
+    }
+
+    public String getSalvoLocationCell() {
+        return salvoLocationCell;
+    }
+
+    public void setSalvoLocationCell(String salvoLocationCell) {
+        this.salvoLocationCell = salvoLocationCell;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @JsonIgnore //prevent infinite looping
+    public Salvo getSalvo() {
+        return salvo;
+    }
+
+    public void setSalvo(Salvo salvo) {
+        this.salvo = salvo;
+    }
+
+}
